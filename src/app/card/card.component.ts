0377f47e3cdacdb7036c022dd4c10d3108e9611e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-04-05 10:33:50 +0330

    feat: add card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { SearchComponent } from '../search/search.component';
+import { GalleryComponent } from '../gallery/gallery.component';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, SearchComponent, GalleryComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
